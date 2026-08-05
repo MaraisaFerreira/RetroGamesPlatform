780858b0dfdd57dbb647e1bc24362c9e3083bf6b
MaraisaFerreira <ferreira.maraisacristina@gmail.com>
2026-08-05 10:54:33 -0300

    refator. change main package name

--- src/main/java/maraisaferreira/com/github/RetroGamePlatform/RetroGamePlatformApplication.java
+++ src/main/java/maraisaferreira/com/github/RetroGamePlatform/RetroGamePlatformApplication.java
@@ -1,4 +1,4 @@
-package com.example.RetroGamePlatform;
+package maraisaferreira.com.github.RetroGamePlatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/maraisaferreira/com/github/RetroGamePlatform/RetroGamePlatformApplicationTests.java
+++ src/test/java/maraisaferreira/com/github/RetroGamePlatform/RetroGamePlatformApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.RetroGamePlatform;
+package maraisaferreira.com.github.RetroGamePlatform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
