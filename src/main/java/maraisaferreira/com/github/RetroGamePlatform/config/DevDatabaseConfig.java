391c7ef174751152b221d4a5408b9ad3f1dc8c31
MaraisaFerreira <ferreira.maraisacristina@gmail.com>
2026-08-05 12:35:55 -0300

    feat. clean dev database on inicialization

---
+++ src/main/java/maraisaferreira/com/github/RetroGamePlatform/config/DevDatabaseConfig.java
@@ -0,0 +1,20 @@
+package maraisaferreira.com.github.RetroGamePlatform.config;
+
+import org.springframework.boot.flyway.autoconfigure.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("dev")
+@Configuration
+public class DevDatabaseConfig {
+
+    @Bean
+    FlywayMigrationStrategy cleanDatabase() {
+        return flyway -> {
+            flyway.clean();
+
+            flyway.migrate();
+        };
+    }
+}
