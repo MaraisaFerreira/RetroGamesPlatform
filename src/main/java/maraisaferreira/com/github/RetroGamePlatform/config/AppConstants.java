4ea4697e6d9343b3630be2b9611cad8f70a171dc
MaraisaFerreira <ferreira.maraisacristina@gmail.com>
2026-08-14 10:33:17 -0300

    chore. alter main file of constants name

---
+++ src/main/java/maraisaferreira/com/github/RetroGamePlatform/config/AppConstants.java
@@ -0,0 +1,9 @@
+package maraisaferreira.com.github.RetroGamePlatform.config;
+
+public final class AppConstants {
+    public static final int PAGE_SIZE = 20;
+
+    private AppConstants() {
+
+    }
+}
