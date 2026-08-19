83e70dea22769edfdfde8d5946b5c8f3f873f64d
MaraisaFerreira <ferreira.maraisacristina@gmail.com>
2026-08-19 11:50:09 -0300

    feat. save birthdate instead age for player

--- src/main/java/maraisaferreira/com/github/RetroGamePlatform/model/Player.java
+++ src/main/java/maraisaferreira/com/github/RetroGamePlatform/model/Player.java
@@ -1,16 +1,19 @@
 package maraisaferreira.com.github.RetroGamePlatform.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Pattern;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import maraisaferreira.com.github.RetroGamePlatform.config.AppConstants;
+import maraisaferreira.com.github.RetroGamePlatform.exceptions.CustomBadRequestException;
+import maraisaferreira.com.github.RetroGamePlatform.exceptions.errorMessages.ExceptionMessages;
 import maraisaferreira.com.github.RetroGamePlatform.exceptions.errorMessages.ValidationMessages;
-import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.UUID;
 
 @Getter
@@ -26,23 +29,35 @@ public class Player {
     @Column(nullable = false)
     private String name;
 
+    @Setter(AccessLevel.NONE)
+    @Column(nullable = false)
+    @Past(message = ValidationMessages.MUST_BE_PAST_DATE)
+    private LocalDate birthDate;
+
     @Pattern(regexp = AppConstants.EMAIL_PATTERN,
             message = ValidationMessages.NOT_A_VALID_EMAIL)
     @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false)
-    @Max(value = 100, message = ValidationMessages.MAX_VALUE_ALLOWED + "100")
-    @Min(value = 5, message = ValidationMessages.LOWER_VALUE_ALLOWED + "5")
-    private Integer age;
-
-    @Column(nullable = false)
     private String password;
 
-    public Player(String name, String email, Integer age, String password) {
+    public Player(String name, LocalDate birthDate, String email, String password) {
         this.name = name;
+        this.setBirthDate(birthDate);
         this.email = email;
-        this.age = age;
         this.password = password;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        if (Period.between(birthDate, LocalDate.now()).getYears() < 10) {
+            throw new CustomBadRequestException(ExceptionMessages.UNDER_AGE);
+        }
+
+        this.birthDate = birthDate;
+    }
+
+    public Integer getAge() {
+        return Period.between(birthDate, LocalDate.now()).getYears();
     }
 }
--- src/main/resources/db/migration/V7__create_players.sql
+++ src/main/resources/db/migration/V7__create_players.sql
@@ -1,11 +1,8 @@
 CREATE TABLE players
 (
-    id       binary(16) PRIMARY KEY,
-    name     VARCHAR(250) NOT NULL,
-    email    VARCHAR(250) NOT NULL UNIQUE,
-    age      INT          NOT NULL,
-    password VARCHAR(255) NOT NULL,
-
-    CONSTRAINT check_age
-        CHECK (age BETWEEN 5 AND 100)
+    id         binary(16) PRIMARY KEY,
+    name       VARCHAR(250) NOT NULL,
+    email      VARCHAR(250) NOT NULL UNIQUE,
+    birth_date DATE         NOT NULL,
+    password   VARCHAR(255) NOT NULL
 );
