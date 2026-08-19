0e7ad7244db6b00f3faca1c342c1eb7ebba89b47
MaraisaFerreira <ferreira.maraisacristina@gmail.com>
2026-08-19 11:50:51 -0300

    feat. save birthdate instead age for player

--- src/main/java/maraisaferreira/com/github/RetroGamePlatform/dto/request/PlayerRequestDto.java
+++ src/main/java/maraisaferreira/com/github/RetroGamePlatform/dto/request/PlayerRequestDto.java
@@ -1,13 +1,22 @@
 package maraisaferreira.com.github.RetroGamePlatform.dto.request;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import maraisaferreira.com.github.RetroGamePlatform.config.AppConstants;
 import maraisaferreira.com.github.RetroGamePlatform.exceptions.errorMessages.ValidationMessages;
 import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public record PlayerRequestDto(
         @NotBlank(message = ValidationMessages.NOT_NULL_OR_EMPTY)
         String name,
+
+        @NotNull(message = ValidationMessages.NOT_NULL_OR_EMPTY)
+        LocalDate birthDate,
 
         @Pattern(regexp = AppConstants.EMAIL_PATTERN, message = ValidationMessages.NOT_A_VALID_EMAIL)
         @NotBlank(message = ValidationMessages.NOT_NULL_OR_EMPTY)
@@ -15,11 +24,10 @@ public record PlayerRequestDto(
 
         @Length(min = 5, message = ValidationMessages.MIN_LENGTH_ALLOWED + "5 chars")
         @NotBlank(message = ValidationMessages.NOT_NULL_OR_EMPTY)
-        String password,
-
-        @Max(value = 100, message = ValidationMessages.MAX_VALUE_ALLOWED + "100")
-        @Min(value = 5, message = ValidationMessages.LOWER_VALUE_ALLOWED + "5")
-        @NotNull(message = ValidationMessages.NOT_NULL_OR_EMPTY)
-        Integer age
+        String password
 ) {
+    @AssertTrue(message = ValidationMessages.INVALID_DATE)
+    public boolean getBirthDateValid() {
+        return birthDate != null && Period.between(this.birthDate, LocalDate.now()).getYears() >= 10;
+    }
 }
