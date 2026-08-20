d4b6e3e9ea043d22933fb9fa59ca23fa878d22a7
MaraisaFerreira <ferreira.maraisacristina@gmail.com>
2026-08-20 10:54:06 -0300

    fix. add final to required attr

---
+++ src/main/java/maraisaferreira/com/github/RetroGamePlatform/security/service/UserService.java
@@ -0,0 +1,25 @@
+package maraisaferreira.com.github.RetroGamePlatform.security.service;
+
+import lombok.RequiredArgsConstructor;
+import maraisaferreira.com.github.RetroGamePlatform.exceptions.CustomNotFoundException;
+import maraisaferreira.com.github.RetroGamePlatform.exceptions.errorMessages.ExceptionMessages;
+import maraisaferreira.com.github.RetroGamePlatform.model.Player;
+import maraisaferreira.com.github.RetroGamePlatform.repositories.PlayerRepository;
+import maraisaferreira.com.github.RetroGamePlatform.security.details.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService implements UserDetailsService {
+    private final PlayerRepository playerRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Player player = playerRepository.findByEmail(email)
+                .orElseThrow(() -> new CustomNotFoundException(ExceptionMessages.notFound("Email")));
+        return new User(player);
+    }
+}
