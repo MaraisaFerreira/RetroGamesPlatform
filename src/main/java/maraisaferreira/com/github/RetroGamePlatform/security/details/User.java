53e8851f7723f0eb8e10fff2a60e1f675ded5251
MaraisaFerreira <ferreira.maraisacristina@gmail.com>
2026-08-20 10:54:54 -0300

    feat. impl UserDetails

---
+++ src/main/java/maraisaferreira/com/github/RetroGamePlatform/security/details/User.java
@@ -0,0 +1,34 @@
+package maraisaferreira.com.github.RetroGamePlatform.security.details;
+
+import lombok.Getter;
+import maraisaferreira.com.github.RetroGamePlatform.model.Player;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+public class User implements UserDetails {
+    private final Player player;
+
+    public User(Player player) {
+        this.player = player;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return player.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return player.getEmail();
+    }
+}
